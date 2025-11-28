d7a8253d8895c9b9dfd1b8ce0a5029f21f4b19f3
heinekmadsen <48906528+heinekmadsen@users.noreply.github.com>
2025-11-28 16:12:29 +0100

    Normalize string quotes and improve code formatting in Genvex V3 component

--- esphome/components/genvexv3/__init__.py
+++ esphome/components/genvexv3/__init__.py
@@ -2,15 +2,16 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
 
-genvexv3_ns = cg.esphome_ns.namespace('genvexv3')
-Genvexv3 = genvexv3_ns.class_('Genvexv3', cg.Component)
+genvexv3_ns = cg.esphome_ns.namespace("genvexv3")
+Genvexv3 = genvexv3_ns.class_("Genvexv3", cg.Component)
 
-CONF_GENVEXV3_ID = 'genvexv3_id'
+# This name must match what genvexv3.climate imports
+CONF_genvexv3_ID = "genvexv3_id"
 
 CONFIG_SCHEMA = cv.Schema({
-    cv.GenerateID(): cv.declare_id(Genvexv3),   
+    cv.GenerateID(): cv.declare_id(Genvexv3),
 })
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    yield cg.register_component(var, config) 
+    yield cg.register_component(var, config)
